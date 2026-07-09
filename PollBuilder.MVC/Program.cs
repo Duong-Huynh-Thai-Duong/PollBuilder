075cd61a55725bb0296967f7129a76c0e34609c7
Thai Duong <duonghuynhthaiduong19@gmail.com>
2026-07-09 14:55:49 +0700

    Setting up Connection String and installed one more Identity UI Package for Infrastructure

--- PollBuilder.MVC/Program.cs
+++ PollBuilder.MVC/Program.cs
@@ -1,7 +1,12 @@
+using PollBuilder.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container. AYE
+// Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// 1. Hook up your Clean Architecture Infrastructure (Database & Identity)
+builder.Services.AddInfrastructureServices(builder.Configuration);
 
 var app = builder.Build();
 
@@ -9,21 +14,19 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
+app.UseStaticFiles();
 app.UseRouting();
 
+// 2. Add Authentication (Must be before Authorization)
+app.UseAuthentication();
 app.UseAuthorization();
-
-app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
-
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
