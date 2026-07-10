25a02a2a414f9299c66d402ff05505a5df7ad8c2
Thai Duong <duonghuynhthaiduong19@gmail.com>
2026-07-10 23:23:23 +0700

    Created Application DTOs and Interface services

---
+++ PollBuilder.Application/DTOs/Polls/CreateQuestionDTO.cs
@@ -0,0 +1,15 @@
+﻿namespace PollBuilder.Application.DTOs.Polls
+{
+    public class CreateQuestionDTO
+    {
+        public string Text { get; set; } = string.Empty;
+
+        // We use an int here that maps to your QuestionType enum (0 = MultipleChoice, 3 = OpenText)
+        public int Type { get; set; }
+
+        public int Position { get; set; }
+
+        // If it's a multiple-choice question, they will send a list of text options
+        public List<string> Options { get; set; } = new List<string>();
+    }
+}
---
+++ PollBuilder.Application/Interfaces/IPollService.cs
@@ -0,0 +1,16 @@
+﻿using PollBuilder.Application.DTOs.Polls;
+
+namespace PollBuilder.Application.Interfaces
+{
+    public interface IPollService
+    {
+        /// <summary>
+        /// Takes the user's input, saves it to the database, and returns the generated 5-character short code.
+        /// This fulfills the "POST /polls" requirement.
+        /// </summary>
+        Task<string> CreatePollAsync(CreatePollDTO createPollDto);
+
+        // Note: We will also need a method here to fetch a poll by its code (GET /polls/{code}),
+        // but we will add that once we create the DTO to send the data back to the user!
+    }
+}
