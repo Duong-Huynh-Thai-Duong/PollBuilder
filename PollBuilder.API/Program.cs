344a6d5c55095de3b63bbc2c9a1ddf36c69e642b
Thai Duong <duonghuynhthaiduong19@gmail.com>
2026-07-13 00:02:15 +0700

    Swagger UI implementation and ResponseDTOs

--- PollBuilder.API/Program.cs
+++ PollBuilder.API/Program.cs
@@ -6,15 +6,20 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddInfrastructureServices(builder.Configuration);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+//builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    //app.MapOpenApi();
 }
 
 app.UseHttpsRedirection();
---
+++ PollBuilder.Application/DTOs/Polls/QuestionResponseDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace PollBuilder.Application.DTOs.Polls
+{
+    public class QuestionResponseDTO
+    {
+        public int Id { get; set; }
+        public string Text { get; set; } = string.Empty;
+        public int Type { get; set; }
+        public int Position { get; set; }
+
+        // This will hold the options belonging to this specific question
+        public List<OptionResponseDTO> Options { get; set; } = new List<OptionResponseDTO>();
+    }
+}
